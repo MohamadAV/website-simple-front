2c30a56cc2764a7b01d01c8df482647543bbccff
Mohamad Amin Vard <vard00780@gmail.com>
2022-10-07 22:19:18 +0330

    logos added.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from "react";
 import Banner from "./components/Banner";
 import Cards from "./components/Cards";
+import { Logos } from "./components/Logos";
 import Navbar from "./components/Navbar";
 import Search from "./components/Search";
 
@@ -12,6 +13,7 @@ class App extends Component {
         <Banner />
         <Cards/>
         <Search/>
+        <Logos/>
       </Fragment>
     );
   }
--- src/components/logos.js
+++ src/components/logos.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import styles from "./css/logos.module.css";
+
+import apple from "../images/apple.jpg";
+import samsung from "../images/samsung.jpg";
+import xiaomi from "../images/xiaomei.jpg";
 
 export const Logos = () => {
   return (
-    <div>logos</div>
-  )
-}
+    <div className={styles.container}>
+      <h3>Who support US?</h3>
+      <div>
+        <img src={apple} alt="logo" />
+        <img src={samsung} alt="logo" />
+        <img src={xiaomi} alt="logo" />
+      </div>
+    </div>
+  );
+};
